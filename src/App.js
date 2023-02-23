bb3c9655ece5511eb38b255bdba9924a91178678
Nick <manlynick@gmail.com>
2023-02-23 02:47:22 -0600

    editted message

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <div style={{color: 'white'}}>
       <ul id='nav-list'>
-        <li><a href='https://ch-skiptracing.vercel.app//skip'>SkipTracer</a></li>
-        <li><a href='https://ch-skiptracing.vercel.app//calc'>OfferCalc</a></li>
+        <li><a href='https://ch-skiptracing.vercel.app/skip'>SkipTracer</a></li>
+        <li><a href='https://ch-skiptracing.vercel.app/calc'>OfferCalc</a></li>
       </ul>
       <AppRouter />
     </div>
